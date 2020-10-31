607908e46c291d087b152f89838ba38d85fa5fea
Swagat Bhattacharjee <66122924+swagat1297@users.noreply.github.com>
2020-10-31 11:58:51 +0530

    First Name added

--- firstname.java
+++ firstname.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class firstname {
 
+	//to check valid first name//
+		public static void CheckFirstName() {
+			Scanner scan = new Scanner(System.in);
+			String read = "[A-Z][a-z]{3,}";
+			
+			System.out.println("Enter the first name:");
+			String firstName = scan.next();
+			String text = firstName;
+			
+			Pattern pattern = Pattern.compile(read);
+			Matcher match = pattern.matcher(text);
+			
+			boolean result = match.matches();
+			System.out.println(result);
+			
+			scan.close();
+			
+		}
 }
